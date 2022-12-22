cc819c9b779d03dd97151bcea57f0c453d840428
Tal Jacob <taljacob2@gmail.com>
2022-12-22 20:25:42 +0200

    Finish AllocationTableList With Valgrind
    Signed-off-by: Tal Jacob <taljacob2@gmail.com>

--- AllocationTableList.c
+++ AllocationTableList.c
@@ -56,6 +56,8 @@ void DestructAllocationTableListNonGeneric(List *allocationTableList) {
                 prevAllocationTable);
     }
 
+    free(allocationTableList->thisObjectBase);
+
     free(allocationTableList);
 }
 
