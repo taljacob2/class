ee9a8b3d4722bd656b740691f70908528679b945
Tal Jacob <taljacob2@gmail.com>
2022-12-26 08:10:59 +0200

    Cleanup main
    Signed-off-by: Tal Jacob <taljacob2@gmail.com>

--- src/main.c
+++ src/main.c
@@ -4,37 +4,9 @@
 
 /// @see how to polymorph https://stackoverflow.com/a/8194632/14427765
 int main() {
-    //    AutoDestructable *autoDestructable = AutoDestructableConstructor();
-
-    //    (*autoDestructable->print)(autoDestructable);
-    //    (*autoDestructable->addOneToX)(autoDestructable);
-    //    (*autoDestructable->print)(autoDestructable);
-    //
-    //    AutoDestructable *class_2 = AutoDestructableConstructor();
-    //    (*class_2->print)();
-    //    (*class_2->addOneToX)();
-    //    (*class_2->print)();
-
-
-    //    autoDestructable->destructable->destructor(autoDestructable);
-
-
-    //    free(class_2);
-
-
-    //    Object *object = ObjectConstructor();
-    //    object->destructable->destructor(object);
-
-    //    AutoDestructable *autoDestructable = AutoDestructableConstructor();
-    //    autoDestructable->object->destructable->destructor(autoDestructable);
-
-
     Circle *circle = CircleConstructor();
 
-//    MemberList *memberList = MemberListConstructor();
-
     CircleV2 *circleV2 = CircleV2Constructor();
-
 
     return 0;
 }
