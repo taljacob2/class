bef8cb724877543f16490bcb6cd7bc5ae69adcae
Tal Jacob <taljacob2@gmail.com>
2022-12-28 01:14:20 +0200

    Add Get Member
    Signed-off-by: Tal Jacob <taljacob2@gmail.com>

--- src/oop/Object/Object.c
+++ src/oop/Object/Object.c
@@ -1,32 +1,181 @@
 #include "Object.r"
 
+/* ------------------------------ GET LIST ---------------------------------- */
+
+// "private" function.
 Legacy_ObjectComponent *getLegacyObject(Object *object) {
     return (Legacy_ObjectComponent *) getAnonymousPointerByIndex(object, 0);
 }
 
+// "private" function.
 Legacy_List *getPrivateMemberNameLegacy_List(Object *object) {
     return (Legacy_List *) getAnonymousPointerByIndex(object, 1);
 }
 
+// "private" function.
 Legacy_List *getPublicMemberNameLegacy_List(Object *object) {
     return (Legacy_List *) getAnonymousPointerByIndex(object, 2);
 }
 
+// "private" function.
 MemberList *getMethodsMemberList(Object *object) {
     return (MemberList *) getAnonymousPointerByIndex(object, 3);
 }
 
+// "private" function.
 MemberList *getConstructorMemberList(Object *object) {
     return (MemberList *) getAnonymousPointerByIndex(object, 4);
 }
 
+// "private" function.
 MemberList *getDestructorMemberList(Object *object) {
     return (MemberList *) getAnonymousPointerByIndex(object, 5);
 }
 
+// "private" function.
 MemberList *getFieldsMemberList(Object *object) {
     return (MemberList *) getAnonymousPointerByIndex(object, 6);
 }
+
+/* ----------------------------- GET MEMBER --------------------------------- */
+
+// "private" function.
+BOOLEAN predicateFindAccessModifierMemberNameByMemberName(
+        const Legacy_Node *nodeThatPointsToMemberName,
+        const char *const  stringToSearch) {
+    return strcmp((char *) (nodeThatPointsToMemberName->data),
+                  stringToSearch) == 0;
+}
+
+// "private" function.
+Legacy_Node *findAccessModifierMemberName(Legacy_List *accessModifierLegacyList,
+                                          char *       memberName) {
+    return accessModifierLegacyList->findNodeByPredicateOfConstString(
+            accessModifierLegacyList,
+            predicateFindAccessModifierMemberNameByMemberName, memberName);
+}
+
+// "private" function.
+Legacy_Node *findPrivateMemberName(Object *object, char *memberName) {
+    return findAccessModifierMemberName(getPrivateMemberNameLegacy_List(object),
+                                        memberName);
+}
+
+// "private" function.
+Legacy_Node *findPublicMemberName(Object *object, char *memberName) {
+    return findAccessModifierMemberName(getPublicMemberNameLegacy_List(object),
+                                        memberName);
+}
+
+// "private" function.
+BOOLEAN isPrivateMember(Object *object, char *memberName) {
+    return findPrivateMemberName(object, memberName) != NULL;
+}
+
+// "private" function.
+BOOLEAN isPublicMember(Object *object, char *memberName) {
+    return findPublicMemberName(object, memberName) != NULL;
+}
+
+// "private" function.
+Legacy_Object *findMemberInMemberListByMemberName(MemberList *memberList,
+                                                  char *      memberName) {
+    return memberList->getMemberByName(memberList, memberName);
+}
+
+// "private" function.
+Legacy_Object *getAccessModifierMember(Legacy_List *accessModifierLegacyList,
+                                       MemberList * memberList,
+                                       char *       memberName) {
+    Legacy_Node *findAccessModifierMemberNameReturnValue =
+            findAccessModifierMemberName(accessModifierLegacyList, memberName);
+    if (findAccessModifierMemberNameReturnValue == NULL) { return NULL; }
+
+    Legacy_Object *findMemberInMemberListByMemberNameReturnValue =
+            findMemberInMemberListByMemberName(memberList, memberName);
+    if (findMemberInMemberListByMemberNameReturnValue == NULL) { return NULL; }
+
+    return findMemberInMemberListByMemberNameReturnValue;
+}
+
+/* ---------------------- Specific Access Modifier -------------------------- */
+
+// "private" function.
+Legacy_Object *getPrivateMember(Object *object, MemberList *memberList,
+                                char *memberName) {
+    return getAccessModifierMember(getPrivateMemberNameLegacy_List(object),
+                                   memberList, memberName);
+}
+
+// "private" function.
+Legacy_Object *getPublicMember(Object *object, MemberList *memberList,
+                               char *memberName) {
+    return getAccessModifierMember(getPublicMemberNameLegacy_List(object),
+                                   memberList, memberName);
+}
+
+/* ------------ Specific Access Modifier & Specific MemberList -------------- */
+
+/* ------------- Methods ------------- */
+
+// "private" function.
+Legacy_Object *getPrivateMethod(Object *object, char *memberName) {
+    return getAccessModifierMember(getPrivateMemberNameLegacy_List(object),
+                                   getMethodsMemberList(object), memberName);
+}
+
+// "private" function.
+Legacy_Object *getPublicMethod(Object *object, char *memberName) {
+    return getAccessModifierMember(getPublicMemberNameLegacy_List(object),
+                                   getMethodsMemberList(object), memberName);
+}
+
+/* ------------- Constructor ------------- */
+
+// "private" function.
+Legacy_Object *getPrivateConstructor(Object *object, char *memberName) {
+    return getAccessModifierMember(getPrivateMemberNameLegacy_List(object),
+                                   getConstructorMemberList(object),
+                                   memberName);
+}
+
+// "private" function.
+Legacy_Object *getPublicConstructor(Object *object, char *memberName) {
+    return getAccessModifierMember(getPublicMemberNameLegacy_List(object),
+                                   getConstructorMemberList(object),
+                                   memberName);
+}
+
+/* ------------- Destructor ------------- */
+
+// "private" function.
+Legacy_Object *getPrivateDestructor(Object *object, char *memberName) {
+    return getAccessModifierMember(getPrivateMemberNameLegacy_List(object),
+                                   getDestructorMemberList(object), memberName);
+}
+
+// "private" function.
+Legacy_Object *getPublicDestructor(Object *object, char *memberName) {
+    return getAccessModifierMember(getPublicMemberNameLegacy_List(object),
+                                   getDestructorMemberList(object), memberName);
+}
+
+/* ------------- Fields ------------- */
+
+// "private" function.
+Legacy_Object *getPrivateField(Object *object, char *memberName) {
+    return getAccessModifierMember(getPrivateMemberNameLegacy_List(object),
+                                   getFieldsMemberList(object), memberName);
+}
+
+// "private" function.
+Legacy_Object *getPublicField(Object *object, char *memberName) {
+    return getAccessModifierMember(getPublicMemberNameLegacy_List(object),
+                                   getFieldsMemberList(object), memberName);
+}
+
+/* ----------------------------- ADD MEMBER --------------------------------- */
+
 
 /// TODO: public. TODO: test if we can invoke the `destruct` multiple times and
 ///     it will be still okay. maybe rename to something secret.
