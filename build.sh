f26bb737ed3d2e33f43ad7cdbea7b6053ae2c330
Tal Jacob <taljacob2@gmail.com>
2022-12-27 11:56:15 +0200

    EDIT README.md With Neww CLASS
    Signed-off-by: Tal Jacob <taljacob2@gmail.com>

--- build.sh
+++ build.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-# Build "oop.a".
-#PWD=$(pwd)
-#cd src/oop
-#./build-library.sh
-#cd "$PWD"
-
 # Build program.
 gcc -g -Wall \
 src/main.c \
@@ -14,15 +8,3 @@ src/CircleV2.c \
 src/CircleV4.c \
 src/CircleV5.c \
 -L. src/oop/oop.a
-
-
-## Compile *.c to *.o.
-#for file in *.c; do
-#
-#  # Continue if file was not found.
-#  [ -f "$file" ] || continue
-#
-#  # Do something.
-#
-#  gcc -c "$file" "$file.o"
-#done
