319e5119f494908e23ca75ea5ce97dc873afc080
Tal Jacob <taljacob2@gmail.com>
2022-12-28 09:20:56 +0200

    Add concat
    Signed-off-by: Tal Jacob <taljacob2@gmail.com>

--- src/oop/Object/Concat.c
+++ src/oop/Object/Concat.c
@@ -1,3 +1,25 @@
-//
-// Created by Tal on 28-Dec-22.
-//
+#include "Concat.h"
+
+/**
+ * @attention Remember to `free` the return value.
+ *
+ * For example:
+ *
+ * @code
+ * int main(void) {
+ *   const char *concattedString = concat("Hi", "There");
+ *   puts(concattedString);
+ *   free((void *)concattedString);
+ *   return 0;
+ * }
+ * @endcode
+ */
+const char *concat(const char *firstString, const char *secondString) {
+    const size_t sizeOfFirstString = strlen(firstString) + (sizeof(char) * 1);
+    char *returnValue = malloc(strlen(firstString) + strlen(secondString) +
+                               (sizeof(char) * 1));
+    memcpy(returnValue, firstString, sizeOfFirstString);
+    strcat(returnValue, secondString);
+
+    return returnValue;
+}
--- src/oop/Object/Concat.h
+++ src/oop/Object/Concat.h
@@ -1,6 +1,9 @@
 #ifndef CONCAT_H
 #define CONCAT_H
 
+#include <stdlib.h>
+#include <string.h>
+
 #define __CONCAT_NOT_EXPANDED__(A, B) A##B
 #define CONCAT(A, B)                  __CONCAT_NOT_EXPANDED__(A, B)
 #define CONCAT_SURROUND(middle, whatToSurround) \
