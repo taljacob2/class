c73b9ebf7433a7020ee6bfa3d71566b52aeef9b0
Tal Jacob <taljacob2@gmail.com>
2022-12-28 12:53:19 +0200

    Add Doc
    Signed-off-by: Tal Jacob <taljacob2@gmail.com>

--- src/oop/AnonymousObject/AnonymousObject.c
+++ src/oop/AnonymousObject/AnonymousObject.c
@@ -25,14 +25,18 @@
  * @endcode
  *
  * @return
+ *
+ * @see https://stackoverflow.com/questions/24850392/is-there-a-way-to-get-the-value-of-line-on-one-line-and-use-that-value-on-ot
+ * @see More About Preventing A Macro-Expanding With This Trick:
+ *      https://stackoverflow.com/a/11694231/14427765
  */
 Object *createAnonymousObject() {
     //    const char* thisAnonymousObjectClassName = QUOTE(ANONYMOUS_OBJECT___CONST_CLASS_NAME);
     //    DEFINE_CLASS_H(thisAnonymousObjectClassName)
     //
 
-    CREATE_LOCAL_CONST_STRING_VAR_FROM_MACRO_TEXT(
-            thisAnonymousObjectClassName, ANONYMOUS_OBJECT___CONST_CLASS_NAME)
+//    CREATE_LOCAL_CONST_STRING_VAR_FROM_MACRO_TEXT(
+//            thisAnonymousObjectClassName, ANONYMOUS_OBJECT___CONST_CLASS_NAME)
 
 
     DEFINE_CLASS_H(ANONYMOUS_OBJECT___CONST_CLASS_NAME)
