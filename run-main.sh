ba1ae9ef867ae843ea9dcbd7942b5a1d9d10b608
Tal Jacob <taljacob2@gmail.com>
2022-12-29 09:47:53 +0200

    Fix `run-main.sh` and `run-test.sh`
    Signed-off-by: Tal Jacob <taljacob2@gmail.com>

--- run-main.sh
+++ run-main.sh
@@ -3,7 +3,7 @@
 source ./config.sh
 
 ## Run main program.
-#./"$MAIN_PATH"/a.out
+#"$MAIN_PATH"/a.out
 
-# Run main program with Valgrind.
-valgrind --leak-check=full --show-leak-kinds=all ./"$MAIN_PATH"/a.out
+## Run main program with Valgrind.
+#valgrind --leak-check=full --show-leak-kinds=all "$MAIN_PATH"/a.out
--- run-test.sh
+++ run-test.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+source ./config.sh
+
 ## Run test program.
-#./"$TEST_PATH"/a.out
+#"$TEST_PATH"/a.out
 
 # Run test program with Valgrind.
-valgrind --leak-check=full --show-leak-kinds=all ./"$TEST_PATH"/a.out
+valgrind --leak-check=full --show-leak-kinds=all "$TEST_PATH"/a.out
