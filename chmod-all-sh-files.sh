c55ac9fcb981f32f7f82b70c11246969be1ef55b
Tal Jacob <taljacob2@gmail.com>
2022-12-31 12:12:43 +0200

    Temporary save
    Signed-off-by: Tal Jacob <taljacob2@gmail.com>

--- chmod-all-sh-files.sh
+++ chmod-all-sh-files.sh
@@ -7,14 +7,77 @@ source ./progress-bar.sh
 # --------------------------- Edit to you liking: ------------------------------
 
 # Relative to `$ROOT_PATH`.
-EXCLUDED_DIRECTORIES=(".git" ".github")
+EXCLUDED_WILDCARDS=(".git" ".github")
 
 # ---------------------------------- Code --------------------------------------
+
+contains () {
+    sourceList="$1"
+    valueToSearch="$2"
+
+    for i in "${sourceList[@]}" ; do
+        if [ "$i" == "$valueToSearch" ] ; then
+            echo true
+            return
+        fi
+    done
+
+    echo false
+    return
+}
+
+deleteSmallListFromLargeList () {
+    largeList="$1"
+    smallList="$2"
+
+    index=0
+    for element in "${largeList[@]}" ; do
+        if [ $(contains "${smallList[@]}" "$element") == true ] ; then
+            unset largeList[$index]
+        fi
+        ((index++))
+    done
+
+    echo "${largeList[@]}"
+}
+
+
+EXCLUDED_WILDCARDS=(".git" ".github" "*.bash")
+
+
+# find `pwd` -type f -print
+
+ROOT_PATH=`pwd`
+
+# find `pwd` -type f -print -not -name "$EXCLUDED_WILDCARDS"
+# find `pwd` -type f -not -wholename "*.zip"
+
+# Set excludedFiles
+excludedFiles=()
+for wildCard in "${EXCLUDED_WILDCARDS[@]}" ; do
+    excludedFiles+=($(find "$ROOT_PATH" -type f -wholename "$ROOT_PATH/$wildCard"))
+done
+
+# Add all files.
+allFiles=$(find "$ROOT_PATH" -type f)
+
+# Set includedFiles
+includedFiles=$(deleteSmallListFromLargeList "${allFiles[@]}" "${excludedFiles[@]}")
+
+unset allFiles
+unset excludedFiles
+
+# For debugs
+# printf 'includedFiles = %s\n' ${includedFiles[@]}
+
+exit 0
+
+# --------------------------------------------------------------------------
 
 cd "$ROOT_PATH"
 
 
-allFilesAndDirectoriesRecursivelySinceRootPathAsOneLongString=$(find . -type f -print)
+allFilesAndDirectoriesRecursivelySinceRootPathAsOneLongString=$(find "$ROOT_PATH" -type f -print)
 
 # Convert long string to string list.
 allFilesAndDirectoriesRecursivelySinceRootPathAsListOfStrings=($allFilesAndDirectoriesRecursivelySinceRootPathAsOneLongString)
@@ -25,15 +88,15 @@ for f in "${allFilesAndDirectoriesRecursivelySinceRootPathAsListOfStrings[@]}"; 
 
   # Print progress bar.
   printProgressBarOnceWithCalculatedPercentToPrint "$progressIndex" "$allFilesAndDirectoriesRecursivelySinceRootPathSize"
-  progressIndex=$((progressIndex + 1))
+  ((progressIndex++))
 
   # "$f" without first 2 chars. (They are `./`).
   file=`echo "$f" | awk '{print substr($0,3)}'`
   fullPathOfFile="$ROOT_PATH/$file"
 
-  # if "$f" is contained within "${EXCLUDED_DIRECTORIES[@]}"
-  for excludedFileOrDirectory in "${EXCLUDED_DIRECTORIES[@]}"; do
-      if echo "$file" | grep "^$excludedFileOrDirectory" ; then
+  # if "$f" is contained within "${EXCLUDED_WILDCARDS[@]}"
+  for excludedFileOrDirectory in "${EXCLUDED_WILDCARDS[@]}"; do
+      if echo "$file" | grep "^$excludedFileOrDirectory$" ; then
           # TODO: debug
           echo "excluded file found: $file"
           continue
