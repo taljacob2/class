d85f05eed4f7bef8b5439716c0dfcc10930c2897
Tal Jacob <taljacob2@gmail.com>
2023-01-03 18:18:52 +0200

    Update main.c
    Signed-off-by: Tal Jacob <taljacob2@gmail.com>

--- src/main/main.c
+++ src/main/main.c
@@ -5,11 +5,11 @@
 #include "CircleV5.h"
 
 int main() {
-    Circle *circle = CircleConstructor();
-
-    CircleV2 *circleV2 = CircleV2Constructor();
-
-    MemberList *memberList = MemberListConstructor("MemberList");
+//    Circle *circle = CircleConstructor();
+//
+//    CircleV2 *circleV2 = CircleV2Constructor();
+//
+//    MemberList *memberList = MemberListConstructor("MemberList");
 
     //    methodsMemberList->methodsMemberList->destructable->destructor(methodsMemberList);
 
@@ -34,15 +34,19 @@ int main() {
     //    legacyMemberList->legacyObjectComponent->destructable->destructor(legacyMemberList);
 
 
-//    MemberList * memberList1 = MemberListConstructor();
+    MemberList * memberList1 = MemberListConstructor();
+
+    memberList1->addMemberWhichIsLegacy_Object(memberList1, "legacyList",
+            (Legacy_Object *) Legacy_ListConstructor());
+
 //    memberList1->legacyObjectComponent->destructable->destructor(memberList1);
-//
+
 //    Legacy_List *legacyList = Legacy_ListConstructor();
 //    legacyList->legacyObjectComponent->destructable->destructor(legacyList);
 
 
 
-        Object *object= construct("Object");
+//        Object *object= construct("Object");
 
 
     //    // TODO: FIXME:
