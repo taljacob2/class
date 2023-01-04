311d422a51d0ba2e3212c54778118df4e5c555b1
Tal Jacob <taljacob2@gmail.com>
2023-01-04 10:52:53 +0200

    Add -Wpedantic To GCC
    Signed-off-by: Tal Jacob <taljacob2@gmail.com>

--- build-test.sh
+++ build-test.sh
@@ -3,7 +3,7 @@
 source ./config.sh
 
 # Build test program.
-gcc -g -Wall \
+gcc -g -Wall -Wpedantic \
 "$TEST_PATH"/main.c \
 -L. "$OUTPUT_LIB_PATH"/"$OUTPUT_LIB_FILE_NAME.a" \
 -o "$TEST_PATH"/a.out
