dc8f928438081b76ceaa54ae563654eedd17a2d2
Tal Jacob <taljacob2@gmail.com>
2023-01-12 17:26:19 +0200

    Need To Fix getDoubleRValueMember
    Signed-off-by: Tal Jacob <taljacob2@gmail.com>

--- src/main/CircleV7.c
+++ src/main/CircleV7.c
@@ -3,12 +3,14 @@
 CircleV7 *CircleV7Constructor() {
     Object *instance = ObjectConstructor("CircleV7");
 
-    instance->addMemberValue(instance, PUBLIC, FIELD, "radius",
-                             INTEGER_RVALUE_AS_OBJECT(2.34));
+    //    instance->addMemberValue(instance, PUBLIC, FIELD, "radius",
+    //                             INTEGER_RVALUE_AS_OBJECT(2.34));
+
+    instance->addDoubleRValueMember(instance, PUBLIC, FIELD, "radius", 2.34);
 
     // TODO: debug
-    printf("%f\n",
-           (float) instance->getMemberValue(instance, PUBLIC, FIELD, "radius"));
+    printf("%f\n", (float) instance->getDoubleRValueMember(instance, PUBLIC,
+                                                           FIELD, "radius"));
 
     return (CircleV7 *) instance;
 }
