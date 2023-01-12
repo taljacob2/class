019a35dce1352ec3c85492b6398dde7622fdadab
Tal Jacob <taljacob2@gmail.com>
2023-01-12 15:58:19 +0200

    Fix AtomicLValue
    Signed-off-by: Tal Jacob <taljacob2@gmail.com>

--- src/lib/oop/Atomic/AtomicLValue.c
+++ src/lib/oop/Atomic/AtomicLValue.c
@@ -16,32 +16,49 @@ extern Legacy_Object *
 getPrivateFieldAndRemoveFromPrivateAccessModifierAndFieldsMemberList(
         Object *object, Object *objectThatContainsThisObjectAsAMember);
 
-extern void setMemberName(Object *object, const char *memberName);
-
-extern const char *getMemberName(Object *object);
+extern Legacy_Object *
+getPrivateFieldAndRemoveFromPrivateAccessModifierAndFieldsMemberListProtected(
+        char *memberName, Object *objectThatContainsThisObjectAsAMember);
 
 /* ----------------------------- Implementation ----------------------------- */
 
+const unsigned char *getATOMIC_MEMBER_NAME(AtomicLValue *atomicLValue) {
+    Legacy_Node *legacyNodeThatContainsATOMIC_MEMBER_NAME =
+            (Legacy_Node *) atomicLValue->getMemberValue(
+                    (Object *) atomicLValue, PRIVATE, FIELD,
+                    __ATOMIC_MEMBER_NAME__);
+    return (const unsigned char *)
+            legacyNodeThatContainsATOMIC_MEMBER_NAME->data;
+}
+
 void setData_AtomicLValue(AtomicLValue *atomicLValue,
                           void *        dynamicallyAllocatedData) {
+    const unsigned char *ATOMIC_MEMBER_NAME =
+            getATOMIC_MEMBER_NAME(atomicLValue);
+
     addPrimitivePrivateField((Object *) atomicLValue,
-                             (char *) getMemberName((Object *) atomicLValue),
+                             (char *) ATOMIC_MEMBER_NAME,
                              dynamicallyAllocatedData);
 }
 
 void setDataWhichIsStaticallyAllocated_AtomicLValue(
         AtomicLValue *atomicLValue, void *staticallyAllocatedData) {
+    const unsigned char *ATOMIC_MEMBER_NAME =
+            getATOMIC_MEMBER_NAME(atomicLValue);
+
     addPrimitivePrivateFieldWhichIsStaticallyAllocated(
-            (Object *) atomicLValue,
-            (char *) getMemberName((Object *) atomicLValue),
+            (Object *) atomicLValue, (char *) ATOMIC_MEMBER_NAME,
             staticallyAllocatedData);
 }
 
 void *getData_AtomicLValue(AtomicLValue *atomicLValue) {
+    const unsigned char *ATOMIC_MEMBER_NAME =
+            getATOMIC_MEMBER_NAME(atomicLValue);
+
     Legacy_Object *dataContainer =
             (Legacy_Object *) atomicLValue->getMemberValue(
                     (Object *) atomicLValue, PRIVATE, FIELD,
-                    (char *) getMemberName((Object *) atomicLValue));
+                    (char *) ATOMIC_MEMBER_NAME);
 
     return strcmp(dataContainer->legacyObjectComponent->CLASS_NAME,
                   "Legacy_AtomicFreer") == 0
@@ -50,15 +67,31 @@ void *getData_AtomicLValue(AtomicLValue *atomicLValue) {
 }
 
 void *AtomicLValueDestructor(AtomicLValue *atomicLValue) {
-    Legacy_Object *legacyObjectOfData =
-            getPrivateFieldAndRemoveFromPrivateAccessModifierAndFieldsMemberList(
-                    (Object *) atomicLValue, (Object *) atomicLValue);
 
+    // A.1: Get and remove "lValue memberName" (i.e. `ATOMIC_MEMBER_NAME`).
+    Legacy_Object *legacyObjectOfDataMemberName =
+            getPrivateFieldAndRemoveFromPrivateAccessModifierAndFieldsMemberListProtected(
+                    __ATOMIC_MEMBER_NAME__, (Object *) atomicLValue);
+    const unsigned char *ATOMIC_MEMBER_NAME =
+            legacyObjectOfDataMemberName->legacyObjectComponent->destructable
+                    ->destructor(legacyObjectOfDataMemberName);
+
+    /*
+     * B.1: Get and remove lValue which is located at "lValue memberName"
+     *      (i.e. `Object->privateFieldMemberName(ATOMIC_MEMBER_NAME)`).
+     * B.2: Destruct lValue.
+     */
+    Legacy_Object *legacyObjectOfData =
+            getPrivateFieldAndRemoveFromPrivateAccessModifierAndFieldsMemberListProtected(
+                    (char *) ATOMIC_MEMBER_NAME, (Object *) atomicLValue);
     legacyObjectOfData->legacyObjectComponent->destructable->destructor(
             legacyObjectOfData);
 
-    // `free` this `ATOMIC_MEMBER_NAME` which is located at `Object->memberName`.
-    free((char *) getMemberName((Object *) atomicLValue));
+    /*
+     * A.2: `free` "lValue memberName" which is located at
+     *      `Object->privateFieldMemberName(__ATOMIC_MEMBER_NAME__)`.
+     */
+    free((void *) ATOMIC_MEMBER_NAME);
 
     return ObjectDestructor((Object *) atomicLValue);
 }
@@ -70,7 +103,15 @@ AtomicLValue *AtomicLValueConstructor(void *  data,
 
     // Set this `Object->memberName` to be `ATOMIC_MEMBER_NAME`.
     const unsigned char *ATOMIC_MEMBER_NAME = getRandomString(20);
-    setMemberName((Object *) instance, (const char *) ATOMIC_MEMBER_NAME);
+
+    /*
+     * Add a private field that its memberName is the value of
+     * `__ATOMIC_MEMBER_NAME__`, and its value is the value of
+     * `ATOMIC_MEMBER_NAME`.
+     */
+    addPrimitivePrivateFieldWhichIsStaticallyAllocated(
+            (Object *) instance, __ATOMIC_MEMBER_NAME__,
+            (void *) ATOMIC_MEMBER_NAME);
 
     if (isDataDynamicallyAllocated) {
         setData_AtomicLValue(instance, data);
