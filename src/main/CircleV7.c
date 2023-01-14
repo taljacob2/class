a45288e546a05c5399ffe1e7752b719d28ae83c4
Tal Jacob <taljacob2@gmail.com>
2023-01-14 09:03:36 +0200

    Remove DEBUG TODO Comments Out Of `CircleV7Constructor`
    Signed-off-by: Tal Jacob <taljacob2@gmail.com>

--- src/main/CircleV7.c
+++ src/main/CircleV7.c
@@ -6,16 +6,13 @@ CircleV7 *CircleV7Constructor() {
     //    instance->addMemberValue(instance, PUBLIC, FIELD, "radius",
     //                             INTEGER_RVALUE_AS_OBJECT(2.34));
 
-    // TODO: debug disable
-    instance->addDoubleRValueMember(instance, PUBLIC, FIELD, "radius", 2.34);
 
-    // TODO: debug
+    instance->addDoubleRValueMember(instance, PUBLIC, FIELD, "radius", 2.34);
     printf("%f\n", (double) instance->getDoubleRValueMember(instance, PUBLIC,
                                                             FIELD, "radius"));
 
 
     instance->addIntegerRValueMember(instance, PUBLIC, FIELD, "radiusInt", 5);
-    // TODO: debug
     printf("%d\n", (int) instance->getIntegerRValueMember(instance, PUBLIC,
                                                           FIELD, "radiusInt"));
 
@@ -25,7 +22,6 @@ CircleV7 *CircleV7Constructor() {
     instance->addMemberValue(
             instance, PUBLIC, FIELD, "radiusAllocatedInt",
             (Object *) AtomicLValueConstructor(radiusAllocated, TRUE));
-    // TODO: debug
     printf("%d\n", *((int *) instance->getMemberValue(instance, PUBLIC, FIELD,
                                                       "radiusAllocatedInt")));
 
