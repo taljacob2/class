6662cfe1110e1168ad6c66f784f01c02e554b385
taljacob2 <taljacob2@gmail.com>
2023-01-18 21:51:59 +0200

    Add 32bit Variables
    Signed-off-by: taljacob2 <taljacob2@gmail.com>

--- src/lib/oop/Object/build-lib.ps1
+++ src/lib/oop/Object/build-lib.ps1
@@ -1,8 +1,10 @@
 $PATH_TO_VISUAL_STUDIO="C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\IDE"
 
 $vcvars64=$(Get-ChildItem "$PATH_TO_VISUAL_STUDIO\..\..\VC\Auxiliary\Build\vcvars64.bat")
+$vcvars32=$(Get-ChildItem "$PATH_TO_VISUAL_STUDIO\..\..\VC\Auxiliary\Build\vcvars32.bat")
 
 $cl64=$(Get-ChildItem "$PATH_TO_VISUAL_STUDIO\..\..\VC\Tools\MSVC\*\bin\Hostx64\x64\cl.exe")
+$cl32=$(Get-ChildItem "$PATH_TO_VISUAL_STUDIO\..\..\VC\Tools\MSVC\*\bin\Hostx86\x86\cl.exe")
 
 # --------------------------------
 
