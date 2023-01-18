95879906ad987a906cf38c86b2806283bdfe47c9
Tal Jacob <taljacob2@gmail.com>
2023-01-18 23:30:22 +0200

    Add Additional `$PATH_TO_VISUAL_STUDIO`
    Signed-off-by: Tal Jacob <taljacob2@gmail.com>

--- src/lib/oop/Object/build-lib.ps1
+++ src/lib/oop/Object/build-lib.ps1
@@ -1,4 +1,5 @@
-$PATH_TO_VISUAL_STUDIO="C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\IDE"
+# $PATH_TO_VISUAL_STUDIO="C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\IDE"
+$PATH_TO_VISUAL_STUDIO="D:\Tal\Visual Studio - Community 2019\IDE - Installation\Common7\IDE"
 
 $vcvars64=$(Get-ChildItem "$PATH_TO_VISUAL_STUDIO\..\..\VC\Auxiliary\Build\vcvars64.bat")
 $vcvars32=$(Get-ChildItem "$PATH_TO_VISUAL_STUDIO\..\..\VC\Auxiliary\Build\vcvars32.bat")
