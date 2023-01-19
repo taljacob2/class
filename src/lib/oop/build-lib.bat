88938d0c5cd6e571d381c7428dbadbba5ce6a83e
Tal Jacob <taljacob2@gmail.com>
2023-01-19 10:19:31 +0200

    Backup
    Signed-off-by: Tal Jacob <taljacob2@gmail.com>

--- src/lib/oop/build-lib.bat
+++ src/lib/oop/build-lib.bat
@@ -19,10 +19,6 @@ REM Initialize with nothing.
 SET subLibList=
 
 for /f %%f in ('dir /s/b/a:d') do (
-
-    REM Initialize with nothing.
-    SET SUB_LIB_NAME=""
-
     @REM @REM REM DEBUG
     @REM echo %%f
 
@@ -31,12 +27,16 @@ for /f %%f in ('dir /s/b/a:d') do (
     @REM REM DEBUG
     @REM cd
 
-    REM DEBUG
-    dir /b | findstr /e .lib
+    @REM REM DEBUG
+    @REM dir /b | findstr /e .lib
+
+    REM not sure if needed
+    setlocal EnableDelayedExpansion
 
     REM See https://stackoverflow.com/a/57802962/14427765
     FOR /F %%i IN ('dir /b ^| findstr /e .lib') DO (
-        SET SUB_LIB_NAME=%%i
+        echo %%i
+        SET SUB_LIB_NAME="%%i"
 
         REM DEBUG
         echo %SUB_LIB_NAME%
@@ -45,7 +45,7 @@ for /f %%f in ('dir /s/b/a:d') do (
 
     REM DEBUG
     @REM SET SUB_LIB_NAME=dir /b | findstr /e .lib
-    @REM echo %SUB_LIB_NAME%
+    echo %SUB_LIB_NAME%
 	cd ..
 
     REM add `%%f%/%SUB_LIB_NAME%` to `subLibList`. Notice the preceeding `' '`.
