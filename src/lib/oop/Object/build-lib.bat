99b62ab1ef83ebd033f1dfc52469848253a6e7f7
Tal Jacob <taljacob2@gmail.com>
2023-01-19 06:45:21 +0200

    Import `config.bat` And Fix Call Of `vcvars64`
    Signed-off-by: Tal Jacob <taljacob2@gmail.com>

--- src/lib/oop/Object/build-lib.bat
+++ src/lib/oop/Object/build-lib.bat
@@ -1,26 +1,21 @@
 @echo off
+
+REM IMPORT
+CALL ../../../../config.bat
+
 SETLOCAL
-
-REM SET PATH_TO_VISUAL_STUDIO=C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\IDE
-SET PATH_TO_VISUAL_STUDIO=D:\Tal\Visual Studio - Community 2019\IDE - Installation\Common7\IDE
-
-SET vcvars64=%PATH_TO_VISUAL_STUDIO%\..\..\VC\Auxiliary\Build\vcvars64.bat
-SET vcvars32=%PATH_TO_VISUAL_STUDIO%\..\..\VC\Auxiliary\Build\vcvars32.bat
-
-SET cl_BASE_PATH=%PATH_TO_VISUAL_STUDIO%\..\..\VC\Tools\MSVC\*
-SET cl64=\bin\Hostx64\x64\cl.exe
-SET cl32=\bin\Hostx86\x86\cl.exe
 
 REM ---------------------------------- Code ------------------------------------
 
 SET LIBRARY_NAME="Object"
 
-CALL :ComipleAllCFilesInCurrentDirectory
+CALL :CompileAllCFilesInCurrentDirectory
 EXIT /B %ERRORLEVEL% 
 
 REM ------------------------------- Functions ----------------------------------
 
-:ComipleAllCFilesInCurrentDirectory
+:CompileAllCFilesInCurrentDirectory
+CALL "%vcvars64%"
 for %%f in (*.c) do (
     CALL :RunCl "/c %%f"
 )
