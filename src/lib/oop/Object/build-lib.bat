1ab5bd7f52cb176f706183c5b51ec8edb481d639
Tal Jacob <taljacob2@gmail.com>
2023-01-19 06:26:56 +0200

    Update `build-lib.bat` To Execute `cl.exe` For All `.c` Files
    Signed-off-by: Tal Jacob <taljacob2@gmail.com>

--- src/lib/oop/Object/build-lib.bat
+++ src/lib/oop/Object/build-lib.bat
@@ -1,4 +1,5 @@
 @echo off
+SETLOCAL
 
 REM SET PATH_TO_VISUAL_STUDIO=C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\IDE
 SET PATH_TO_VISUAL_STUDIO=D:\Tal\Visual Studio - Community 2019\IDE - Installation\Common7\IDE
@@ -6,13 +7,25 @@ SET PATH_TO_VISUAL_STUDIO=D:\Tal\Visual Studio - Community 2019\IDE - Installati
 SET vcvars64=%PATH_TO_VISUAL_STUDIO%\..\..\VC\Auxiliary\Build\vcvars64.bat
 SET vcvars32=%PATH_TO_VISUAL_STUDIO%\..\..\VC\Auxiliary\Build\vcvars32.bat
 
-SET cl64=%PATH_TO_VISUAL_STUDIO%\..\..\VC\Tools\MSVC\*\bin\Hostx64\x64\cl.exe
-SET cl32=%PATH_TO_VISUAL_STUDIO%\..\..\VC\Tools\MSVC\*\bin\Hostx86\x86\cl.exe
+SET cl_BASE_PATH=%PATH_TO_VISUAL_STUDIO%\..\..\VC\Tools\MSVC\*
+SET cl64=\bin\Hostx64\x64\cl.exe
+SET cl32=\bin\Hostx86\x86\cl.exe
 
-REM --------------------------------
+REM ---------------------------------- Code ------------------------------------
 
 SET LIBRARY_NAME="Object"
 
-"%vcvars64%"
-"%cl64%"
+CALL :ComipleAllCFilesInCurrentDirectory
+EXIT /B %ERRORLEVEL% 
 
+REM ------------------------------- Functions ----------------------------------
+
+:ComipleAllCFilesInCurrentDirectory
+for %%f in (*.c) do (
+    CALL :RunCl "/c %%f"
+)
+EXIT /B 0
+
+:RunCl
+for /D %%I in ("%cl_BASE_PATH%") do "%%~I%cl64%" %~1
+EXIT /B 0
