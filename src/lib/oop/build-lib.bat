71812a3ec50af436621737723b20d33565179e35
Tal Jacob <taljacob2@gmail.com>
2023-01-19 21:55:46 +0200

    Update `src/lib/oop/build-lib.bat` Imports
    Signed-off-by: Tal Jacob <taljacob2@gmail.com>

--- src/lib/oop/build-lib.bat
+++ src/lib/oop/build-lib.bat
@@ -1,10 +1,18 @@
 @echo off
 
-SETLOCAL
+REM IMPORTANT: `SETLOCAL EnableDelayedExpansion` enables the use of variables
+REM            inside "for loops".
+REM NOTE: to use the variables, you should call them with `!var!` and not the
+REM       usual `%var%`.
+REM See https://stackoverflow.com/a/13805466/14427765
+SETLOCAL EnableDelayedExpansion
+
+REM --------------------------------- Imports ----------------------------------
 
 REM IMPORT
 SET CONFIG=../../../config.bat
 CALL %CONFIG%
+CALL shared-config-local-variables.bat
 
 REM ---------------------------------- Code ------------------------------------
 
@@ -17,13 +25,6 @@ REM See https://www.geeksforgeeks.org/batch-script-iterating-over-an-array/
 
 REM Initialize with nothing.
 SET subLibList=
-
-REM IMPORTANT: `SETLOCAL EnableDelayedExpansion` enables the use of variables
-REM            inside "for loops".
-REM NOTE: to use the variables, you should call them with `!var!` and not the
-REM       usual `%var%`.
-REM See https://stackoverflow.com/a/13805466/14427765
-SETLOCAL EnableDelayedExpansion
 
 for /f %%f in ('dir /s/b/a:d') do (
 	cd "%%f"
