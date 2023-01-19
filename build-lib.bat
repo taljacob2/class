2b3991d4acff28987e70861c254f86a84f9ef4b6
Tal Jacob <taljacob2@gmail.com>
2023-01-19 21:43:17 +0200

    Add `build-lib.bat`
    Signed-off-by: Tal Jacob <taljacob2@gmail.com>

---
+++ build-lib.bat
@@ -0,0 +1,26 @@
+@echo off
+
+REM IMPORTANT: `SETLOCAL EnableDelayedExpansion` enables the use of variables
+REM            inside "for loops".
+REM NOTE: to use the variables, you should call them with `!var!` and not the
+REM       usual `%var%`.
+REM See https://stackoverflow.com/a/13805466/14427765
+SETLOCAL EnableDelayedExpansion
+
+REM --------------------------------- Imports ----------------------------------
+
+REM IMPORT
+SET CONFIG=config.bat
+CALL %CONFIG%
+CALL shared-config-local-variables.bat
+
+REM ---------------------------------- Code ------------------------------------
+
+REM DEBUG
+echo %OUTPUT_LIB_PATH%
+
+cd "%OUTPUT_LIB_PATH%"
+CALL build-lib.bat
+cd "%ROOT_PATH%"
+
+GOTO :EOF
