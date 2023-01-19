3b08de20c0c0e14fb6292cce44e0f0960ebf93fb
Tal Jacob <taljacob2@gmail.com>
2023-01-19 21:57:47 +0200

    Update `src/lib/oop/Atomic/build-lib.bat` And `src/lib/oop/AnonymousObject/build-lib.bat` Imports
    Signed-off-by: Tal Jacob <taljacob2@gmail.com>

--- src/lib/oop/AnonymousObject/build-lib.bat
+++ src/lib/oop/AnonymousObject/build-lib.bat
@@ -1,10 +1,18 @@
 @echo off
 
-SETLOCAL
+REM IMPORTANT: `SETLOCAL EnableDelayedExpansion` enables the use of variables
+REM            inside "for loops".
+REM NOTE: to use the variables, you should call them with `!var!` and not the
+REM       usual `%var%`.
+REM See https://stackoverflow.com/a/13805466/14427765
+SETLOCAL EnableDelayedExpansion
+
+REM --------------------------------- Imports ----------------------------------
 
 REM IMPORT
 SET CONFIG=../../../../config.bat
 CALL %CONFIG%
+CALL shared-config-local-variables.bat
 
 REM ---------------------------------- Code ------------------------------------
 
