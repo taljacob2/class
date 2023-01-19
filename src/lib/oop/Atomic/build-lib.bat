4b769c2607d4e4deba86469e57eb6a1fa2f2c917
Tal Jacob <taljacob2@gmail.com>
2023-01-19 07:41:59 +0200

    Add `build-lib.bat` To All Sub-Modules
    Signed-off-by: Tal Jacob <taljacob2@gmail.com>

---
+++ src/lib/oop/Atomic/build-lib.bat
@@ -0,0 +1,28 @@
+@echo off
+
+SETLOCAL
+
+REM IMPORT
+SET CONFIG=../../../../config.bat
+CALL %CONFIG%
+
+REM ---------------------------------- Code ------------------------------------
+
+SET LIBRARY_NAME=Atomic
+
+CALL :CompileAllCFilesInCurrentDirectory
+CALL :LibAllObjFilesInCurrentDirectory
+GOTO :EOF
+
+REM ------------------------------- Functions ----------------------------------
+
+:CompileAllCFilesInCurrentDirectory
+    CALL "%vcvars64%"
+    for %%f in (*.c) do (
+        CALL %CONFIG% :RunCl "/c %%f"
+    )
+GOTO :EOF
+
+:LibAllObjFilesInCurrentDirectory
+    CALL %CONFIG% :RunLib "/out:%LIBRARY_NAME%.lib *.obj"
+GOTO :EOF
