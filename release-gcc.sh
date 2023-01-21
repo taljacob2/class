bf7584dabe04bb4f258408f356af0d1c7e703700
Tal Jacob <taljacob2@gmail.com>
2023-01-21 14:26:19 +0200

    Working On `release-gcc.sh`
    Signed-off-by: Tal Jacob <taljacob2@gmail.com>

--- release-gcc.sh
+++ release-gcc.sh
@@ -2,9 +2,22 @@
 
 source ./config.sh
 
-echo releasing gcc here...
-mkdir -p release/gcc
+echo releasing gcc...
 
+OOP_DIRECTORY=release/gcc/"$OUTPUT_LIB_FILE_NAME"
+
+rm -f -r "$OOP_DIRECTORY"
+
+mkdir -p "$OOP_DIRECTORY"
+
+./build-lib.sh
+
+cp "$OUTPUT_LIB_PATH"/"$OUTPUT_LIB_FILE_NAME.a" "$OOP_DIRECTORY"
+cp "$OUTPUT_LIB_PATH"/"$OUTPUT_LIB_FILE_NAME.h" "$OOP_DIRECTORY"
+
+cp "$OUTPUT_LIB_PATH"/**/*.c "$OOP_DIRECTORY"
+cp "$OUTPUT_LIB_PATH"/**/*.h "$OOP_DIRECTORY"
+cp "$OUTPUT_LIB_PATH"/**/*.r "$OOP_DIRECTORY"
 
 
 exit  # This line is mandatory for executing this file as a thread.
