26d488dd010a3d75bed7bc5933d71faeeba1defb
Tal Jacob <taljacob2@gmail.com>
2023-01-21 23:10:36 +0200

    `release-gcc.sh` Works
    Signed-off-by: Tal Jacob <taljacob2@gmail.com>

--- release-gcc.sh
+++ release-gcc.sh
@@ -15,9 +15,21 @@ mkdir -p "$OOP_DIRECTORY"
 cp "$OUTPUT_LIB_PATH"/"$OUTPUT_LIB_FILE_NAME.a" "$OOP_DIRECTORY"
 cp "$OUTPUT_LIB_PATH"/"$OUTPUT_LIB_FILE_NAME.h" "$OOP_DIRECTORY"
 
-cp "$OUTPUT_LIB_PATH"/**/*.c "$OOP_DIRECTORY"
-cp "$OUTPUT_LIB_PATH"/**/*.h "$OOP_DIRECTORY"
-cp "$OUTPUT_LIB_PATH"/**/*.r "$OOP_DIRECTORY"
+
+for f in "$OUTPUT_LIB_PATH"/* ; do
+    if [ -d "$f" ]; then
+        directoryName=$(basename "$f")      
+
+        # "$f" is a directory. Will not run if no directories are available.
+
+        # Create that directory in "$OOP_DIRECTORY".
+        mkdir -p "$OOP_DIRECTORY/$directoryName"
+
+        # Copy all header files from the directory.
+        cp "$f"/*.h "$OOP_DIRECTORY"/"$directoryName"
+        cp "$f"/*.r "$OOP_DIRECTORY"/"$directoryName"
+    fi
+done
 
 
 exit  # This line is mandatory for executing this file as a thread.
