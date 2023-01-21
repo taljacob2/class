61ea5f61ae77bbd1ee8f2337754e0cc8105d76d4
Tal Jacob <taljacob2@gmail.com>
2023-01-21 12:36:08 +0200

    Init Empty Release Files
    Signed-off-by: Tal Jacob <taljacob2@gmail.com>

---
+++ release-msvc.bat
@@ -0,0 +1,25 @@
+@echo off
+
+REM IMPORTANT: `SETLOCAL EnableDelayedExpansion` enables the use of variables
+REM            inside "for loops".
+REM NOTE: to use the variables, you should call them with `!var!` and not the
+REM       usual `%var%`.
+REM See https://stackoverflow.com/a/13805466/14427765
+SETLOCAL EnableDelayedExpansion
+
+REM --------------------------------- Imports ----------------------------------
+
+REM IMPORT
+SET CONFIG=config.bat
+CALL %CONFIG%
+CALL shared-config-local-variables.bat
+
+REM ---------------------------------- Code ------------------------------------
+
+
+
+
+del /F shared-config-local-variables.bat >NUL 2>&1
+GOTO :EOF
+
+REM ------------------------------- Functions ----------------------------------
