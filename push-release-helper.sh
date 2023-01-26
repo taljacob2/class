1381e2f5f0a0c56b7779ada1563bed46adb16540
Tal Jacob <taljacob2@gmail.com>
2023-01-26 08:37:35 +0200

    Reorder `push-release-helper.sh` `pushRelease` And Remove Redundant Debug Code
    Signed-off-by: Tal Jacob <taljacob2@gmail.com>

--- push-release-helper.sh
+++ push-release-helper.sh
@@ -7,19 +7,20 @@ body=$1; shift
 draft=$1; shift
 prerelease=$1; shift
 
-pushRelease() {
-    echo $token
-    echo $tagname
-    echo $name
-    echo $body
-    echo $draft
-    echo $prerelease
-    
-    AUTHORIZATION="Authorization: Bearer $token"
-    JSON_POST={\"tag_name\":\"$tagname\",\"target_commitish\":\"master\",\"name\":\"$name\",\"body\":\"$body\",\"draft\":$draft,\"prerelease\":$prerelease,\"generate_release_notes\":true}
+pushRelease() {    
+    local AUTHORIZATION="Authorization: Bearer $token"
+    local JSON_POST={\"tag_name\":\"$tagname\",\"target_commitish\":\"master\",\"name\":\"$name\",\"body\":\"$body\",\"draft\":$draft,\"prerelease\":$prerelease,\"generate_release_notes\":true}
     
 
-    curl -X 'POST' https://api.github.com/repos/taljacob2/oop/releases -H 'Accept: application/vnd.github+json' -H "$AUTHORIZATION" -H 'X-GitHub-Api-Version: 2022-11-28' -d "$JSON_POST"
+    curl \
+    -X 'POST' \
+    https://api.github.com/repos/taljacob2/oop/releases \
+    -H 'Accept: application/vnd.github+json' \
+    -H "$AUTHORIZATION" \
+    -H 'X-GitHub-Api-Version: 2022-11-28' \
+    -d "$JSON_POST"
+
+
 }
 
 # ---------------------------------- Code --------------------------------------
